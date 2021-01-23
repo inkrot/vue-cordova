f7a8c7be430658ef0811e50f9829ebccfddec797
Islam Zaripov <islam.zar@yandex.ru>
2021-01-23 18:33:15 +0300

    UI starter

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
+// Webpack CSS import
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
+// JS import
+import Vue from 'vue';
+import VueOnsen from 'vue-onsenui'
+
 import App from './App'
 import router from './router'
 
+Vue.use(VueOnsen);
+
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,10 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      {
+          path: '/',
+          name: 'HelloWorld',
+          component: HelloWorld
+      }
   ]
 })
